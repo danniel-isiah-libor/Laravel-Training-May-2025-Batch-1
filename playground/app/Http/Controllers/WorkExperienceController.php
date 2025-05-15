497c8386a66a2c5c406c624432db388a47368426
Danniel Libor <dlibor.dev@gmail.com>
2025-05-15 16:02:25 +0800

    Day 2: work experience database

--- playground/app/Http/Controllers/WorkExperienceController.php
+++ playground/app/Http/Controllers/WorkExperienceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\WorkExperience\StoreRequest;
+use App\Models\WorkExperience;
 use Illuminate\Http\Request;
 
 class WorkExperienceController extends Controller
@@ -10,6 +11,49 @@ class WorkExperienceController extends Controller
     public function store(StoreRequest $request)
     {
         $form = $request->validated();
+
+        // option 1:
+        // WorkExperience::create($form);
+
+        // option 2:
+        // WorkExperience::insert([
+        //     [
+        //         'user_id' => $form['user_id'],
+        //         'company_name' => $form['company_name'],
+        //         'position' => $form['position'],
+        //         'start_date' => $form['start_date'],
+        //         'end_date' => $form['end_date'],
+        //         'created_at' => now(),
+        //         'updated_at' => now()
+        //     ],
+        //     [
+        //         'user_id' => $form['user_id'],
+        //         'company_name' => $form['company_name'],
+        //         'position' => $form['position'],
+        //         'start_date' => $form['start_date'],
+        //         'end_date' => $form['end_date'],
+        //         'created_at' => now(),
+        //         'updated_at' => now()
+        //     ],
+        //     [
+        //         'user_id' => $form['user_id'],
+        //         'company_name' => $form['company_name'],
+        //         'position' => $form['position'],
+        //         'start_date' => $form['start_date'],
+        //         'end_date' => $form['end_date'],
+        //         'created_at' => now(),
+        //         'updated_at' => now()
+        //     ],
+        // ]);
+
+        // option 3:
+        $model = new WorkExperience();
+        $model->user_id = $form['user_id'];
+        $model->company_name = $form['company_name'];
+        $model->position = $form['position'];
+        $model->start_date = $form['start_date'];
+        $model->end_date = $form['end_date'];
+        $model->save();
 
         // saving....
         return view('work-experience.show', [
---
+++ playground/app/Models/WorkExperience.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WorkExperience extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'company_name',
+        'position',
+        'start_date',
+        'end_date',
+    ];
+}
